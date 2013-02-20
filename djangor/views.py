d239c48857aff53a2e82b32382c8ba1b9d236fd1
Eric Chou <echou@ubuntu.(none)>
2013-02-19 20:07:44 -0800

    added tag filtering

--- djangor/views.py
+++ djangor/views.py
@@ -4,6 +4,8 @@ from django.views.generic import CreateView
 from djangor.forms import EntryForm
 from djangor.models import Entry
 
+from django.shortcuts import render_to_response
+import re
 
 class AddEntryView(CreateView):
     """Generic view for creating a new entry
@@ -25,3 +27,10 @@ class AddEntryView(CreateView):
         msg = "Your post, %s, has been published"
         messages.add_message(self.request, messages.INFO, msg % self.object)
         return super(AddEntryView, self).form_valid(form)
+
+def tag_search(request, *args, **kwargs):
+    for i in args:
+        tag = i
+    entries = Entry.objects.filter(tags__name__in=[tag])
+    return render_to_response('djangor/entry_list.html', {'tag': tag, 'entries': entries })
+    
